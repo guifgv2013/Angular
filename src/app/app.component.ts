260a899c054a364d9d99c65aefe68d6d9b80593c
Guilherme Ribeiro <jcrfcal@hotmail.com>
2020-12-06 11:54:45 -0300

    Angula expression - Atributos {{}} e []

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Projeto Angular';
+  url = 'https://optclean.com.br/wp-content/uploads/2018/04/imagem-fundo-transparente-photoshop-1280x720.jpg';
 }
